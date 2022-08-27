3cfeca2294b82332bb875d42a76c9fb5fa36c954
shin101 <shinj0127@gmail.com>
2022-08-27 00:35:00 -0700

    set db address

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ from seed import setup
 CURR_USER_KEY = "curr_user"
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///bodybook'
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///bodybook')
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 app.config['SECRET_KEY'] = 'capstone_project'
